754887dcf12e63f84022e1caa6cfd3e1c7ac1b59
Harry F <harryf8989@gmail.com>
2024-09-20 13:30:43 +1200

    added documentation to HazardModal

--- src/components/Modal/HazardModal.jsx
+++ src/components/Modal/HazardModal.jsx
@@ -4,36 +4,48 @@ import { useSkidModal } from './Skid/SkidModalContext';
 import tinycolor from 'tinycolor2';
 import PropTypes from 'prop-types';
 
+/**
+ * HazardModal component displays detailed information about a selected hazard.
+ * It allows users to review hazards and navigate back to related modals.
+ * 
+ * @component
+ * @param {Object} props - React props
+ * @param {Object} props.selectedHazard - The selected hazard object
+ * @returns {JSX.Element} The rendered component
+ */
 function HazardModal({ selectedHazard }) {
   const { skidModalState, setSkidModalState } = useSkidModal();
 
+  // Parse the harms associated with the selected hazard
   const selectedHarms = selectedHazard.harms ? JSON.parse(selectedHazard.harms) : {};
 
   /**
-   * Closes the Hazard Modal and opens the Skid Modal by updating the state.
+   * Closes the Hazard Modal and opens the appropriate Skid Modal by updating the state.
    * @function handleClose
    * @returns {void}
    */
   const handleClose = () => {
-    if(skidModalState.isSelectHazardsGeneral) {
-      setSkidModalState((prevState) => ({ ...prevState, hazardModalVisible: false, isGeneralHazardsModalVisible: true}));
+    if (skidModalState.isSelectHazardsGeneral) {
+      setSkidModalState((prevState) => ({ ...prevState, hazardModalVisible: false, isGeneralHazardsModalVisible: true }));
     } else if (skidModalState.isSkidModalEdit || skidModalState.isSkidModalAdd) {
-      setSkidModalState((prevState) => ({ ...prevState, hazardModalVisible: false, isSkidModalVisible: true}));
+      setSkidModalState((prevState) => ({ ...prevState, hazardModalVisible: false, isSkidModalVisible: true }));
     } else {
-      setSkidModalState((prevState) => ({ ...prevState, hazardModalVisible: false}));
+      setSkidModalState((prevState) => ({ ...prevState, hazardModalVisible: false }));
     }
   };
-  const lighterBackgroundColor = tinycolor(selectedHazard.color).lighten(30).toString()
+
+  // Lightens the background color of the modal based on the selected hazard's color
+  const lighterBackgroundColor = tinycolor(selectedHazard.color).lighten(30).toString();
 
   return (
     <div data-testid="hazard-modal">
       <Modal
-      show={skidModalState.hazardModalVisible}
-      onHide={handleClose}
-      size='lg'
-      contentClassName='hazard-modal-content'
-      className='hazard-modal'
-      style={{padding: 0, margin: 0 }}
+        show={skidModalState.hazardModalVisible}
+        onHide={handleClose}
+        size='lg'
+        contentClassName='hazard-modal-content'
+        className='hazard-modal'
+        style={{ padding: 0, margin: 0 }}
       >
         <Modal.Header style={{ backgroundColor: selectedHazard.color }} data-testid='hazard-header' closeButton>
           <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
@@ -66,17 +78,18 @@ function HazardModal({ selectedHazard }) {
           </dl>
         </Modal.Body>
         <Modal.Footer>
-            <em>
-              Reviewed: {selectedHazard.reviewDate} ({selectedHazard.reviewReason})
-            </em>
+          <em>
+            Reviewed: {selectedHazard.reviewDate} ({selectedHazard.reviewReason})
+          </em>
         </Modal.Footer>
       </Modal>
     </div>
   );
 }
 
+// Prop type validation for the component's props
 HazardModal.propTypes = {
-  selectedHazard: PropTypes.object.isRequired
+  selectedHazard: PropTypes.object.isRequired // selectedHazard must be an object and is required
 }
 
 export default HazardModal;
