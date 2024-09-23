33baef0b32a7c0cc49ccc1d90bceb6b6eb71b28b
Harry F <harryf8989@gmail.com>
2024-09-23 19:35:18 +1200

    add tests and documentation for UploadPdfModal

--- src/components/Modal/UploadPdfModal.jsx
+++ src/components/Modal/UploadPdfModal.jsx
@@ -1,119 +1,128 @@
+/**
+ * @file UploadPdfModal.jsx
+ * @description Component for uploading a PDF map file within a modal dialog. 
+ * It includes form validation, file upload functionality, and integrates with 
+ * the backend to upload and store the PDF file.
+ */
+
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Modal, Button, Spinner } from 'react-bootstrap';
-///import { useSkidModal } from './Skid/SkidModalContext';
 import { useAlertMessage } from '../AlertMessage';
 import { useMap } from '../Map/MapContext';
 import DragAndDropUpload from '../DragAndDropUpload';
-import { getPresignedUrl, uploadToPresignedUrl, getFilePathFromUrl } from '../../hooks/useFileUpload';
+import {
+  getPresignedUrl,
+  uploadToPresignedUrl,
+  getFilePathFromUrl
+} from '../../hooks/useFileUpload';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import PropTypes from 'prop-types';
-const UploadPdfModal = ({_account, show, setShow}) => {
-  //const { skidModalState, setSkidModalState } = useSkidModal();
-  const { setAlertMessageState } = useAlertMessage();
+
+/**
+ * UploadPdfModal component provides a UI for users to upload a PDF map file.
+ * It uses Formik for form state management and validation, and axios for handling
+ * file uploads to the backend.
+ *
+ * @component
+ * @param {Object} props - React props.
+ * @param {string} props._account - The account identifier used to generate the presigned URL for the upload.
+ * @param {boolean} props.show - Controls the visibility of the modal.
+ * @param {function} props.setShow - Function to set the visibility of the modal.
+ *
+ * @returns {JSX.Element} Rendered component.
+ */
+const UploadPdfModal = ({ _account, show, setShow }) => {
+  const { addToast } = useAlertMessage();
   const { setMapState } = useMap();
+
+  // Local state to manage the selected file and upload error message.
   const [selectedFile, setSelectedFile] = useState(null);
   const [uploadError, setUploadError] = useState('');
 
+  /**
+   * Handles the file selection and sets the selected file state.
+   *
+   * @param {File} file - The file object selected by the user.
+   */
   const handleMapUpload = (file) => {
     setSelectedFile(file);
     setUploadError('');
-  }
+  };
 
+  /**
+   * Resets the file selection and clears the upload error.
+   */
   const removeMapFile = () => {
     setSelectedFile(null);
     setUploadError('');
-  }
+  };
 
+  /**
+   * Submits the form and handles the file upload to the backend.
+   *
+   * @async
+   * @param {string} pdfName - The name of the PDF file provided by the user.
+   */
   const handleSubmit = async (pdfName) => {
-    const id = new Date().getTime();
-    const [presignedUrl, key] = await getPresignedUrl(_account._account._account+"/maps", selectedFile.type);
-    const filePath = getFilePathFromUrl(presignedUrl);
-    
     try {
+      // Generate a presigned URL for uploading the file
+      const [presignedUrl, key] = await getPresignedUrl(`${_account}/maps`, selectedFile.type);
+      const filePath = getFilePathFromUrl(presignedUrl);
+
+      // Upload the file to the generated presigned URL
       await uploadToPresignedUrl(presignedUrl, selectedFile, selectedFile.type);
 
+      // Create a FormData object to send file data to the backend
       const formData = new FormData();
       formData.append('file', selectedFile);
       formData.append('id', pdfName);
       formData.append('url', filePath);
       formData.append('key', key);
 
+      // Send a POST request to the backend to save the file details
       // eslint-disable-next-line no-undef
-      const response = await axios.post(process.env.REACT_APP_URL + '/loadpdf', formData, { withCredentials: true });
+      const response = await axios.post(`${process.env.REACT_APP_URL}/loadpdf`, formData, {
+        withCredentials: true
+      });
 
       if (response.status === 200) {
-        setAlertMessageState((prevState) => ({
-          ...prevState,
-          toasts: [
-            ...prevState.toasts,
-            {
-              id: id,
-              heading: 'Upload Map',
-              show: true,
-              message: `Success! ${pdfName} has been uploaded.`,
-              background: 'success',
-              color: 'white'
-            }
-          ]
-        }));
+        addToast('Upload Map', `Success! ${pdfName} has been uploaded.`, 'success', 'white');
 
+        // Update the map state with the newly uploaded map and set it as the current map URL
         setMapState((prevState) => ({
           ...prevState,
           maps: response.data.maps,
           currentMapUrl: filePath
         }));
 
-        /* setSkidModalState((prevState) => ({
-          ...prevState,
-          isUploadMapModalVisible: false
-        })); */
+        // Close the modal after successful upload
         setShow(false);
       }
     } catch (error) {
-      setAlertMessageState((prevState) => ({
-        ...prevState,
-        toasts: [
-          ...prevState.toasts,
-          {
-            id: id,
-            heading: 'Upload Map',
-            show: true,
-            message: `Error! An error occurred while uploading ${pdfName}. Please try again.`,
-            background: 'danger',
-            color: 'white'
-          }
-        ]
-      }));
+      addToast(
+        'Upload Map',
+        `Error! An error occurred while uploading ${pdfName}. Please try again.`,
+        'danger',
+        'white'
+      );
       console.error('Error submitting form:', error);
     } finally {
-      setSelectedFile(null);
-      setTimeout(() => {
-        setAlertMessageState((prevState) => ({
-          ...prevState,
-          toasts: prevState.toasts.filter((toast) => toast.id !== id)
-        }));
-      }, 10000);
+      setSelectedFile(null); // Reset selected file after submission
     }
   };
 
+  /**
+   * Handles the modal close action and resets the selected file state.
+   */
   const handleClose = () => {
-    /* setSkidModalState((prevState) => ({
-      ...prevState,
-      isUploadMapModalVisible: false
-    })); */
     setShow(false);
     setSelectedFile(null);
   };
 
   return (
-    <Modal
-      show={show}
-      onHide={handleClose}
-      backdrop="static"
-      centered
-    >
+    <Modal show={show} onHide={handleClose} backdrop="static" centered>
       <Modal.Header className="modal-header" closeButton>
         <h5 className="modal-title" id="uploadMapModalLabel">
           Upload Map
@@ -123,7 +132,7 @@ const UploadPdfModal = ({_account, show, setShow}) => {
         <Formik
           initialValues={{ pdfName: '' }}
           validationSchema={Yup.object({
-            pdfName: Yup.string().max(20, 'Too Long!').required('Please provide a valid Map Name.'),
+            pdfName: Yup.string().max(20, 'Too Long!').required('Please provide a valid Map Name.')
           })}
           onSubmit={(values, { setSubmitting }) => {
             if (!selectedFile) {
@@ -133,24 +142,16 @@ const UploadPdfModal = ({_account, show, setShow}) => {
             }
             handleSubmit(values.pdfName);
             setSubmitting(false);
-          }}
-        >
+          }}>
           {({ isSubmitting }) => (
             <Form id="map-upload-form">
               <div className="mb-3">
                 <label htmlFor="pdfName">Map Name:</label>
-                <Field
-                  type="text"
-                  className="form-control"
-                  id="pdfName"
-                  name="pdfName"                  
-                />
+                <Field type="text" className="form-control" id="pdfName" name="pdfName" />
                 <ErrorMessage name="pdfName" component="div" className="text-danger" />
               </div>
               <div className="mb-3">
-                <label htmlFor="map-upload">
-                  Upload PDF Map
-                </label>
+                <label htmlFor="map-upload">Upload PDF Map</label>
                 <DragAndDropUpload
                   id="map-upload"
                   setSelectedFile={handleMapUpload}
@@ -161,11 +162,7 @@ const UploadPdfModal = ({_account, show, setShow}) => {
                 />
               </div>
               <Modal.Footer>
-                <Button
-                  variant="primary"
-                  type="submit"
-                  disabled={isSubmitting}
-                >
+                <Button variant="primary" type="submit" disabled={isSubmitting}>
                   {isSubmitting ? (
                     <>
                       <Spinner
@@ -191,9 +188,20 @@ const UploadPdfModal = ({_account, show, setShow}) => {
 };
 
 UploadPdfModal.propTypes = {
+  /**
+   * The account identifier used for generating the presigned URL.
+   */
   _account: PropTypes.any,
+
+  /**
+   * Boolean indicating if the modal should be visible.
+   */
   show: PropTypes.bool.isRequired,
+
+  /**
+   * Function to set the visibility of the modal.
+   */
   setShow: PropTypes.func.isRequired
-}
+};
 
 export default UploadPdfModal;
--- src/tests/UploadPdfModal.test.js
+++ src/tests/UploadPdfModal.test.js
@@ -1,233 +1,207 @@
-import React from "react";
+import React from 'react';
 import { render, screen, within, waitFor } from '@testing-library/react';
-import axios from "axios";
-import UploadPdfModal from "../components/Modal/UploadPdfModal";
-import { useSkidModal } from "../components/Modal/Skid/SkidModalContext";
-import { useAlertMessage } from "../components/AlertMessage";
-import { useMap } from "../components/Map/MapContext";
-import { setup } from "../utils/testSetup"
-import * as fileUploadHooks from "../hooks/useFileUpload"
+import axios from 'axios';
+import UploadPdfModal from '../components/Modal/UploadPdfModal';
+import { useAlertMessage } from '../components/AlertMessage';
+import { useMap } from '../components/Map/MapContext';
+import { setup } from '../utils/testSetup';
+import * as fileUploadHooks from '../hooks/useFileUpload';
 // Mock hooks and axios
 jest.mock('axios');
-jest.mock('../components/Modal/Skid/SkidModalContext');
 jest.mock('../components/AlertMessage');
 jest.mock('../components/Map/MapContext');
 
 // Mocks for the hooks
-const mockSetSkidModalState = jest.fn();
-const mockSetAlertMessageState = jest.fn();
+const mockAddToast = jest.fn();
 const mockSetMapState = jest.fn();
 
-const alertMessageState = { toasts: [] };
-
 describe('UploadPdfModal', () => {
-    beforeEach(() => {
-        jest.clearAllMocks();
+  beforeEach(() => {
+    jest.clearAllMocks();
 
-        useSkidModal.mockReturnValue({
-            skidModalState: { isUploadMapModalVisible: true},
-            setSkidModalState: mockSetSkidModalState
-        });
-        
-        useAlertMessage.mockReturnValue({
-            alertMessageState: alertMessageState,
-            setAlertMessageState: mockSetAlertMessageState
-        });
-        
-        useMap.mockReturnValue({
-            mapState: { currentMapUrl: ''},
-            setMapState: mockSetMapState
-        });        
+    useAlertMessage.mockReturnValue({
+      addToast: mockAddToast
     });
 
-    test('renders UploadMapModal when isUploadMapModalVisible is true', () => {
-        render(<UploadPdfModal _account={'1'} />);
-        expect(screen.getByRole('heading', {
-            name: /upload map/i
-          })).toBeInTheDocument();
-        expect(screen.getByRole('button', {
-            name: /close/i
-          })).toBeInTheDocument();
-        expect(screen.getByRole('textbox')).toBeInTheDocument();
-        expect(screen.getByText(/upload pdf map/i)).toBeInTheDocument();
-        expect(screen.getByRole('presentation')).toBeInTheDocument();
-        const dialog = screen.getByRole('dialog');
-        within(dialog).getByRole('button', {
+    useMap.mockReturnValue({
+      mapState: { currentMapUrl: '' },
+      setMapState: mockSetMapState
+    });
+  });
+
+  test('renders UploadMapModal when isUploadMapModalVisible is true', () => {
+    render(<UploadPdfModal _account={'1'} show={true} setShow={jest.fn()} />);
+    expect(
+      screen.getByRole('heading', {
+        name: /upload map/i
+      })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole('button', {
+        name: /close/i
+      })
+    ).toBeInTheDocument();
+    expect(screen.getByRole('textbox')).toBeInTheDocument();
+    expect(screen.getByText(/upload pdf map/i)).toBeInTheDocument();
+    expect(screen.getByRole('presentation')).toBeInTheDocument();
+    const dialog = screen.getByRole('dialog');
+    within(dialog).getByRole('button', {
+      name: /upload/i
+    });
+  });
+
+  test('closes UploadMapModal when closed', async () => {
+    const mockSetShow = jest.fn();
+
+    const { user } = setup(<UploadPdfModal _account={'1'} show={true} setShow={mockSetShow} />);
+    const closeButton = screen.getByRole('button', {
+      name: /close/i
+    });
+
+    user.click(closeButton);
+
+    await waitFor(() => {
+      expect(mockSetShow).toHaveBeenCalledWith(false);
+    });
+  });
+
+  test('validates the form inputs - Map Name', async () => {
+    const mockSetShow = jest.fn();
+    const { user } = setup(<UploadPdfModal _account={'1'} show={true} setShow={mockSetShow} />);
+
+    const dialog = screen.getByRole('dialog');
+
+    const uploadButton = within(dialog).getByRole('button', {
+      name: /upload/i
+    });
+
+    user.click(uploadButton);
+
+    await waitFor(() => {
+      expect(screen.getByText('Please provide a valid Map Name.')).toBeInTheDocument();
+    });
+  });
+
+  test('validates the form inputs - Map Name (Too Long)', async () => {
+    const { user } = setup(<UploadPdfModal _account={'1'} show={true} setShow={jest.fn()} />);
+
+    const mapNameInput = screen.getByLabelText(/map name:/i);
+    await user.type(mapNameInput, 'amapnamethatismorethan20characters');
+    const dialog = screen.getByRole('dialog');
+
+    const uploadButton = within(dialog).getByRole('button', {
+      name: /upload/i
+    });
+
+    user.click(uploadButton);
+
+    await waitFor(() => {
+      expect(screen.getByText('Too Long!')).toBeInTheDocument();
+    });
+  });
+
+  test('validates the form inputs - Selected File', async () => {
+    const { user } = setup(<UploadPdfModal _account={'1'} show={true} setShow={jest.fn()} />);
+
+    const mapNameInput = screen.getByLabelText(/map name:/i);
+    await user.type(mapNameInput, 'filenameaaa');
+    const dialog = screen.getByRole('dialog');
+
+    const uploadButton = within(dialog).getByRole('button', {
+      name: /upload/i
+    });
+
+    user.click(uploadButton);
+
+    await waitFor(() => {
+      expect(screen.getByText('Please upload a PDF file.')).toBeInTheDocument();
+    });
+  });
+
+  test('submits the form successfully', async () => {
+    // Mock successful upload
+    axios.post.mockResolvedValue({ status: 200, data: { maps: ['map1', 'map2'] } });
+
+    // Mock the getPresignedUrl function
+    jest
+      .spyOn(fileUploadHooks, 'getPresignedUrl')
+      .mockResolvedValue(['https://example.com/presigned-url', 'mockKey']);
+
+    // Mock the uploadToPresignedUrl function
+    jest.spyOn(fileUploadHooks, 'uploadToPresignedUrl').mockResolvedValue();
+    const { user } = setup(<UploadPdfModal _account={'1'} show={true} setShow={jest.fn()} />);
+
+    const mapNameInput = screen.getByLabelText(/map name:/i);
+    await user.type(mapNameInput, 'filenameaaa');
+    const fileInput = screen.getByText(/upload pdf map/i);
+    await user.upload(
+      fileInput,
+      new File(['dummy content'], 'example.pdf', { type: 'application/pdf' })
+    );
+    const dialog = screen.getByRole('dialog');
+
+    await user.click(
+      within(dialog).getByRole('button', {
         name: /upload/i
-        })
+      })
+    );
+
+    await waitFor(() => {
+      expect(axios.post).toHaveBeenCalledWith(
+        expect.stringContaining('/loadpdf'),
+        expect.any(FormData),
+        { withCredentials: true }
+      );
+
+      expect(mockAddToast).toHaveBeenCalledWith(
+        'Upload Map',
+        'Success! filenameaaa has been uploaded.',
+        'success',
+        'white'
+      );
     });
+  });
 
-    test('closes UploadMapModal when closed', async() => {
-        const { user } = setup(<UploadPdfModal _account={'1'}/>);
-        const closeButton = screen.getByRole('button', {
-            name: /close/i
-        });
+  test('handles a server error (500) during form submission', async () => {
+    // Mock server error
+    axios.post.mockRejectedValue({ response: { status: 500 } });
 
-        user.click(closeButton);
+    // Mock the getPresignedUrl function
+    jest
+      .spyOn(fileUploadHooks, 'getPresignedUrl')
+      .mockResolvedValue(['https://example.com/presigned-url', 'mockKey']);
 
-        await waitFor(() => {
-            expect(mockSetSkidModalState).toHaveBeenCalledWith(expect.any(Function));
+    // Mock the uploadToPresignedUrl function
+    jest.spyOn(fileUploadHooks, 'uploadToPresignedUrl').mockResolvedValue();
 
-            const stateUpdateFn = mockSetSkidModalState.mock.calls[0][0];
+    const { user } = setup(<UploadPdfModal _account={'1'} show={true} setShow={jest.fn()} />);
 
-            const newState = stateUpdateFn({currentMapUrl: ''});
-            expect(newState).toEqual(expect.objectContaining({
-                isUploadMapModalVisible: false
-            }))
+    const mapNameInput = screen.getByLabelText(/map name:/i);
+    await user.type(mapNameInput, 'filenameaaa');
+    const fileInput = screen.getByText(/upload pdf map/i);
+    await user.upload(
+      fileInput,
+      new File(['dummy content'], 'example.pdf', { type: 'application/pdf' })
+    );
+    const dialog = screen.getByRole('dialog');
 
-        })
-    })
-    
-    test('validates the form inputs - Map Name', async() => {
-        const { user } = setup(<UploadPdfModal _account={'1'}/>);
+    await user.click(within(dialog).getByRole('button', { name: /upload/i }));
 
-        const dialog = screen.getByRole('dialog');
+    await waitFor(() => {
+      // Verify axios post call
+      expect(axios.post).toHaveBeenCalledWith(
+        expect.stringContaining('/loadpdf'),
+        expect.any(FormData),
+        { withCredentials: true }
+      );
 
-        const uploadButton = within(dialog).getByRole('button', {
-        name: /upload/i
-        });
-
-        user.click(uploadButton);
-
-        await waitFor(() => {
-            expect(screen.getByText('Please provide a valid Map Name.')).toBeInTheDocument();
-        });
+      // Check the new state for the correct error toast message
+      expect(mockAddToast).toHaveBeenCalledWith(
+        'Upload Map',
+        'Error! An error occurred while uploading filenameaaa. Please try again.',
+        'danger',
+        'white'
+      );
     });
-
-    test('validates the form inputs - Map Name (Too Long)', async() => {
-        const { user } = setup(<UploadPdfModal _account={'1'}/>);
-
-        const mapNameInput = screen.getByLabelText(/map name:/i);
-        await user.type(mapNameInput, 'amapnamethatismorethan20characters');
-        const dialog = screen.getByRole('dialog');
-
-        const uploadButton = within(dialog).getByRole('button', {
-        name: /upload/i
-        });
-
-        user.click(uploadButton);
-
-        await waitFor(() => {
-            expect(screen.getByText('Too Long!')).toBeInTheDocument();
-        });
-    });
-
-    test('validates the form inputs - Selected File', async() => {
-        const { user } = setup(<UploadPdfModal _account={'1'}/>);
-
-        const mapNameInput = screen.getByLabelText(/map name:/i);
-        await user.type(mapNameInput, 'filenameaaa');
-        const dialog = screen.getByRole('dialog');
-
-        const uploadButton = within(dialog).getByRole('button', {
-        name: /upload/i
-        });
-
-        user.click(uploadButton);
-
-        await waitFor(() => {
-            expect(screen.getByText('Please upload a PDF file.')).toBeInTheDocument();
-        });
-    });
-    test('submits the form successfully', async () => {
-
-        // Mock successful upload
-        axios.post.mockResolvedValue({ status: 200, data: { maps: ['map1', 'map2'] } });
-        
-        // Mock the getPresignedUrl function
-        jest.spyOn(fileUploadHooks, 'getPresignedUrl').mockResolvedValue(['https://example.com/presigned-url', 'mockKey']);
-
-        // Mock the uploadToPresignedUrl function
-        jest.spyOn(fileUploadHooks, 'uploadToPresignedUrl').mockResolvedValue();
-        const { user } = setup(<UploadPdfModal _account={'1'}/>);
-
-        const mapNameInput = screen.getByLabelText(/map name:/i);
-        await user.type(mapNameInput, 'filenameaaa');
-        const fileInput = screen.getByText(/upload pdf map/i)
-        await user.upload(fileInput, new File(['dummy content'], 'example.pdf', { type: 'application/pdf' }));
-        const dialog = screen.getByRole('dialog');
-       
-        await user.click( within(dialog).getByRole('button', {
-            name: /upload/i
-        }));
-
-        await waitFor(() => {
-            expect(axios.post).toHaveBeenCalledWith(
-                expect.stringContaining('/loadpdf'),
-                expect.any(FormData),
-                {withCredentials: true}
-            );
-
-            expect(mockSetAlertMessageState).toHaveBeenCalledWith(expect.any(Function));
-
-            const stateUpdateFn = mockSetAlertMessageState.mock.calls[0][0];
-
-            const newState = stateUpdateFn(alertMessageState);
-
-            expect(newState).toEqual(expect.objectContaining({
-                toasts: expect.arrayContaining([
-                    expect.objectContaining({
-                        heading: 'Upload Map',
-                        show: true,
-                        message: 'Success! filenameaaa has been uploaded.',
-                        background: 'success',
-                        color: 'white'
-                    })
-                ])
-            }));
-
-        })
-
-    });
-
-    test('handles a server error (500) during form submission', async () => {
-        // Mock server error
-        axios.post.mockRejectedValue({ response: { status: 500 } });
-        
-        // Mock the getPresignedUrl function
-        jest.spyOn(fileUploadHooks, 'getPresignedUrl').mockResolvedValue(['https://example.com/presigned-url', 'mockKey']);
-    
-        // Mock the uploadToPresignedUrl function
-        jest.spyOn(fileUploadHooks, 'uploadToPresignedUrl').mockResolvedValue();
-        
-        const { user } = setup(<UploadPdfModal _account={'1'}/>);
-    
-        const mapNameInput = screen.getByLabelText(/map name:/i);
-        await user.type(mapNameInput, 'filenameaaa');
-        const fileInput = screen.getByText(/upload pdf map/i);
-        await user.upload(fileInput, new File(['dummy content'], 'example.pdf', { type: 'application/pdf' }));
-        const dialog = screen.getByRole('dialog');
-       
-        await user.click(within(dialog).getByRole('button', { name: /upload/i }));
-    
-        await waitFor(() => {
-            // Verify axios post call
-            expect(axios.post).toHaveBeenCalledWith(
-                expect.stringContaining('/loadpdf'),
-                expect.any(FormData),
-                { withCredentials: true }
-            );
-    
-            // Verify setAlertMessageState call for the error case
-            expect(mockSetAlertMessageState).toHaveBeenCalledWith(expect.any(Function));
-    
-            // Simulate the state update function being called and apply it
-            const stateUpdateFn = mockSetAlertMessageState.mock.calls[0][0];
-            const newState = stateUpdateFn(alertMessageState);
-    
-            // Check the new state for the correct error toast message
-            expect(newState).toEqual(expect.objectContaining({
-                toasts: expect.arrayContaining([
-                    expect.objectContaining({
-                        heading: 'Upload Map',
-                        show: true,
-                        message: 'Error! An error occurred while uploading filenameaaa. Please try again.',
-                        background: 'danger',
-                        color: 'white'
-                    })
-                ])
-            }));
-        });
-    });
-    
-})
+  });
+});
